253c0295767d03b97d98f77dd6e924ce300e6b2f
xel <xel1995@163.com>
2019-08-13 11:16:24 +0800

    update pic pack to bin script

--- src/Ai_Module_MF1_lib_test/ui/pic/pic/gen_imgbin.sh
+++ src/Ai_Module_MF1_lib_test/ui/pic/pic/gen_imgbin.sh
@@ -21,4 +21,23 @@ done
 INIT_PATH="."
 ergodic $INIT_PATH
 
+#delete exist file
+if [ -e pic.kfpkg ]
+then
+    rm pic.kfpkg
+fi
+
+#compress bin to kfpkg
 zip pic.kfpkg flash-list.json 0.bin 1.bin 2.bin 3.bin 4.bin 5.bin 6.bin
+
+if [ "$1" ] ;then
+    echo "merge kfpkg"
+
+    #merge kfpkg to bin
+    python3 merge_kfpkg.py pic.kfpkg
+    rm pic.kfpkg
+    # addr file save_file
+    python3 pack_kfpkg.py 12582912 pic.bin pic.kfpkg
+fi
+
+rm *.bin
